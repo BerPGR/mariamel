9435907c9e8760ee0a721010e833fe3d4b8146f7
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2026-02-11 17:05:15 -0300

    file url

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import * as path from 'path'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: path.resolve(__dirname, "src")
+    alias: {
+       "@": fileURLToPath(new URL('./src', import.meta.url))
       }
     ]
   }
